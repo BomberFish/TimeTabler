6dba0597ef37d711b23a14ac2ec67a2c709518e9
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-08-31 16:23:17 -0400

    Initial Commit

---
+++ TimeTabler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeTabler
+//
+//  Created by Hariz Shirazi on 2023-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeTabler/TimeTablerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTablerApp.swift
+//  TimeTabler
+//
+//  Created by Hariz Shirazi on 2023-08-31.
+//
+
+import SwiftUI
+
+@main
+struct TimeTablerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
